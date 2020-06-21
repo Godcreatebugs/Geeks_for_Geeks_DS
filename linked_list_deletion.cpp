8d2b402ed23cddbe02648fdbaab21f078715cfc8
Godcreatebugs <bhuvapiyush63@gmail.com>
2020-06-21 15:40:17 -0400

    Own Solution implemented

--- linked_list_deletion.cpp
+++ linked_list_deletion.cpp
@@ -7,46 +7,65 @@ public:
   int data;
   Node* next;
 };
-// To delete a node from linked list, we need to do following steps.
-// 1) Find previous node of the node to be deleted.
-// 2) Change the next of previous node.
-// 3) Free memory for the node to be deleted.
-
-//mind you-- you just have key and hence a full blown O(n) check is needed from beginning
-void DeleteElement(Node** head_ref,int key){
-// creating a tmp vaiable which hold head_pointers value to naviagte
-// and to delete the elemnt we also need previous_node value
-Node* tmp = *head_ref; // *head_ref is derefrencing the head_ref and this gives us head_pointer pointing to very first element
-Node* previous; //to link previous to next and here we know where we are
-
+/* To delete a node from linked list :
+1. We need access to previous_element to the node that has to be deleted and key for the element to be deleted.
+2. And as we dont know the pointer to element that needs to be deleted we have to iterate and this has to be O(n) operation.
+3. We have to create a temp node(which is like must have when we are iterating)
+*/
 //if condition if we have found the key on the first node then we dont have to iterate through while loop
-if (tmp!=NULL && tmp->data==key){
-  //tmp and head_pointer are both pointing in the same direcction
-    *head_ref= tmp->next;// Head pointer will move towards the next element
-    free(tmp); //delete temp pointer
-    return;
-    // Dont have idea wether that deletes actual node or not
-}//if
 
-while (tmp !=NULL  && tmp->data==key){
-  prev = tmp // moving temp to previous and temp to ahead
-  temp =temp->next;
-}
-if (tmp ==NULL) return; // if we havent found the key and reached at the end of linkedlist
-//or else we have found key then you unlink tmp and free it and previous should point to next element in linkedlist
-prev->next = tmp->next;
-free(tmp);//free up tmp pointer
+void deleteElement(Node** head_ref,int key){
+  //head_ref for begining and key to compare the data in nodes
+  Node* temp = *head_ref; //derefrence the head_ref to get head_pointer
+  Node* previous_node =NULL; //previous_node to keep track
 
+  if (temp==NULL) return; //nothing to delete from null linkedlist
+  else{
+    while (temp !=NULL && temp->data != key){
+      previous_node = temp;
+      temp = temp->next;
+    }// the second condition checks wether if we have reached last element return nothing.
+    if (temp->data == key && temp != NULL){
+      previous_node->next = temp->next;
+      free(temp);
+    }
+    else return;
+  }
 }
 
 //print function for the pointer
 void PrintList(Node *n){
-  while (n->next !=NULL) {
-    cout<<n->data;
+  while (n!=NULL) {
+    cout<<n->data <<" ";
     n = n->next;
   }
+  cout<<"\n";
 }
 
+//Drivers code
+//COMMENT--->>It works fine
 int main(){
-  
+    Node* head = NULL;
+    Node* first =NULL;
+    Node* second =NULL;
+    //Populating the nodes
+    // A GREAT MISTAKE THAT YOU HAVE DONE HERE WHICH YOU ARE LIKELY TO REPEAT IS
+    // YOU SPECIFIED POINTERS BUT NOT ACTUAL MEEMORY ON THE HEAP
+    head = new Node();
+    first = new Node();
+    second = new Node();
+
+    head->data =1;
+    head->next =first;
+
+    first->data = 2;
+    first->next = second;
+
+    second->data =3;
+    second->next =NULL;
+    PrintList(head);
+    deleteElement(&head,3);
+    PrintList(head);
+    return 0;
+
 }
